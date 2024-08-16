8066723589bbbcee32ee2d3a9c947c0aa9dbb840
MrWhiteShapi <aigunovshapi@yandex.ru>
2024-08-16 19:19:42 +0300

    First commit. Добавлен модуль(микросервис)

---
+++ notification/src/main/java/ru/practicum/notification/ShareItNotification.java
@@ -0,0 +1,13 @@
+package ru.practicum.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareItNotification {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareItNotification.class, args);
+	}
+
+}
--- template/src/main/java/ru/practicum/shareit/model/dto/user/UserDto.java
+++ template/src/main/java/ru/practicum/shareit/model/dto/user/UserDto.java
@@ -2,6 +2,8 @@ package ru.practicum.shareit.model.dto.user;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 
